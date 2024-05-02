b69c3adbd2e380e91c7750f6d862d2daae25cdc5
NathanMarquis <nathanmarquis23@gmail.com>
2024-05-01 19:09:52 -0600

    Creating template for  creating users, coop, and chickens

---
+++ Main/Coop.java
@@ -0,0 +1,9 @@
+public class Coop {
+    private String coopName;
+    private Chicken myChicken;
+
+    public Coop(String name) {
+
+    }
+    
+}
---
+++ Main/User.java
@@ -0,0 +1,27 @@
+public class User {
+    private String userName;
+    private String password;
+    private Coop myCoop;
+    // database entry for user
+
+    public User( String name, String password)  {
+        this.userName = name;
+        this.password = password;
+        this.myCoop = new Coop(name);
+        // create database selection for user
+    }
+
+    // Methods
+
+    /*
+     * Set username
+    */
+
+    /*
+     * Set password
+    */  
+
+    /*
+     * Delete account
+     */
+}
